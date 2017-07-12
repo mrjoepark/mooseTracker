3e4b86b899bf9f1ba5f62aa87be087820310bbc8
mrjoepark <joe.park20@gmail.com>
2017-07-11 20:04:55 -0500

    Update main.py
    Need to add last LED and GPIO code

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from xml.dom import minidom
+import RPi.GPIO as GPIO
 import urllib2
 from time import sleep
 import pygame
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(8,GPIO.OUT)
+GPIO.setup(7,GPIO.OUT)
+GPIO.setup(4,GPIO.OUT)
+GPIO.setup(3,GPIO.OUT)
+GPIO.setup(2,GPIO.OUT)
 
 pygame.mixer.init()
 chant=pygame.mixer.Sound('/Users/JoePark/Desktop/Royals.wav')
@@ -12,24 +20,34 @@ def GPIO_hrs():
     print bin_shr
     #turn inning lights on or off from bin_current_inning_list 
     if bin_shr[0]== '1':
+        GPIO.output(8,1)
         print ' first on'
     else:
+        GPIO.output(8,0)
         print ' first off'
     if bin_shr[1]== '1':
+        GPIO.output(7,1)
         print ' second on'
     else:
+        GPIO.output(7,0)
         print ' second off'
     if bin_shr[2]== '1':
+        GPIO.output(4,1)
         print ' third on'
     else:
+        GPIO.output(4,0)
         print ' third off'
     if bin_shr[3]== '1':
+        GPIO.output(3,1)
         print ' fourth on'
     else:
+        GPIO.output(3,0)
         print ' fourth off'
     if bin_shr[4]== '1':
+        GPIO.output(2,1)
         print ' fifth on'
     else:
+        GPIO.output(2,0)
         print ' fifth off'
     if bin_shr[5]== '1':
         print ' sixth on'
